1906d0c978d07b76a53e0e1d03cddfacebaed312
kovrik <kovrik0@gmail.com>
2016-10-21 20:15:07 +1300

    Further cleanup

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,18 @@ fn main() {
     opts.optflag("x", "two-byte-hex",   "Two-byte hexadecimal display");
     opts.optflag("d", "two-byte-dec",   "Two-byte decimal display");
     opts.optflag("o", "two-byte-octal", "Two-byte octal display");
-    let matches = match opts.parse(&args[1..]) {
+    let args = match opts.parse(&args[1..]) {
         Ok(m)  => { m }
         Err(f) => { panic!(f.to_string()) }
     };
-    if matches.opt_present("h") {
+    if args.opt_present("h") {
         print_usage();
         return;
     }
 
     // offset in bytes
-    let offset: usize = if matches.opt_present("s") {
-        match matches.opt_str("s").unwrap().parse::<usize>() {
+    let offset: usize = if args.opt_present("s") {
+        match args.opt_str("s").unwrap().parse::<usize>() {
             Ok(s)  => s,
             Err(s) => panic!("hexdumpr: failed to parse offset: {}", s),
         }
@@ -45,8 +45,8 @@ fn main() {
     };
 
     // get filename
-    let filename = if !matches.free.is_empty() {
-        matches.free[0].clone()
+    let filename = if !args.free.is_empty() {
+        args.free[0].clone()
     } else {
         print_usage();
         return;
@@ -56,48 +56,31 @@ fn main() {
     let mut f = File::open(&filename).expect("Unable to open file");
     let mut data = Vec::new();
     f.read_to_end(&mut data).expect("Unable to read data");
-
-    if offset > data.len() {
-        return;
-    }
+    if data.len() < offset { return }
 
     // length in bytes
     let mut end = data.len();
-    if matches.opt_present("n") {
-        let length = match matches.opt_str("n").unwrap().parse::<usize>() {
+    if args.opt_present("n") {
+        let length = match args.opt_str("n").unwrap().parse::<usize>() {
                          Ok(s)  => s,
                          Err(s) => panic!("hexdumpr: failed to parse length: {}", s),
                      };
-        
         if length < data.len() - offset {
             end = offset + length;
         }
     }
-    if end == 0 {
-        return;
-    }
+    if end == 0 { return }
 
     // display mode
-    let mut bytes = 2;
-    let mut display = 'x';
-    if matches.opt_present("b") {
-        display = 'b';
-        bytes = 1;
-    } else if matches.opt_present("c") {
-        display = 'c';
-        bytes = 1;
-    } else if matches.opt_present("C") {
-        display = 'C';
-        bytes = 1;
-    } else if matches.opt_present("x") {
-        display = 'x';
-        bytes = 2;
-    } else if matches.opt_present("d") {
-        display = 'd';
-        bytes = 2;
-    } else if matches.opt_present("o") {
-        display = 'o';
-        bytes = 2;
+    let bytes;
+    let display;
+    match () {
+        _ if args.opt_present("b") => { display = 'b'; bytes = 1; }
+        _ if args.opt_present("c") => { display = 'c'; bytes = 1; }
+        _ if args.opt_present("C") => { display = 'C'; bytes = 1; }
+        _ if args.opt_present("d") => { display = 'd'; bytes = 2; }
+        _ if args.opt_present("o") => { display = 'o'; bytes = 2; }
+        _                          => { display = 'x'; bytes = 2; }
     }
     print_hexdump(&data[offset..end], offset, display, bytes);
 }
